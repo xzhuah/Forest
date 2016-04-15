8a536faf4b40e6b9ce44cd2469c0d12b7fdfba5d
acewhite <ace.bai@lalamove.com>
2016-04-15 22:00:17 +0800

    config

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var APP_ID = process.env.LC_APP_ID;
 var APP_KEY = process.env.LC_APP_KEY;
 var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
 
-AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
+AV.initialize('QdSwHCdXnUjjLLGhodgIWhe5-gzGzoHsz', 'bBT9v34EJ8hN6b4jpUre1YeF');
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
 //AV.Cloud.useMasterKey();
 
