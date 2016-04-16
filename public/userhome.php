7b1d2ca3d88308aaf0394441134f5e00b6e5de0a
acewhite <ace.bai@lalamove.com>
2016-04-17 00:28:58 +0800

    https fix

--- public/userhome.php
+++ public/userhome.php
@@ -44,7 +44,7 @@ $counter=0;
     <link href="css/style.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.js"></script>
     <style>
         pre {
             border: 1px grey dotted;
